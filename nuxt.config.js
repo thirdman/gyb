d6f9c000e3d5d47d5ea25cb2a2dcfe299c12876d
Gareth Bedford <g@thirdman.co.nz>
2022-01-18 16:27:29 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,11 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "https://pandaearth.online/preview.png",
+      },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
